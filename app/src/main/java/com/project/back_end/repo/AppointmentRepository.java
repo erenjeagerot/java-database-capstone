08fbe849e43233e1dfadb71e93f12ce85f73c296
Eren <erenalkanx@outlook.com>
2026-08-01 13:19:51 +0300

    Add appointment repository queries

---
+++ app/src/main/java/com/project/back_end/repo/AppointmentRepository.java
@@ -0,0 +1,13 @@
+package com.project.back_end.repo;
+
+import com.project.back_end.models.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    List<Appointment> findByDoctor_IdAndAppointmentTimeBetween(Long doctorId, LocalDateTime start, LocalDateTime end);
+}
